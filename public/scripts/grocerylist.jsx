d66ff4a301a715507754a18fcc9c22a6b01df0de
pokemane <j.d.cravener@gmail.com>
2015-10-21 22:26:25 -0400

    I have somehow created an infinite loop in line 20 of the grocerylist.jsx file. I get an "exceeded max call stack size" when the client tries running this but I have no idea how it is happening or why.

--- public/scripts/grocerylist.jsx
+++ public/scripts/grocerylist.jsx
@@ -33,32 +33,82 @@ var GroceryList = React.createClass({
 });
 
 var GroceryForm = React.createClass({
+ handleSubmit: function(e){
+  e.preventDefault();
+  var name = this.refs.name.value.trim();
+  var comment = this.refs.comment.value.trim();
+  var price = this.refs.price.value();
+  if (!name || !price){
+   return;
+  }
+  this.props.onGrocerySubmit({name: name, price: price, comment: comment});
+  this.refs.name.value = '';
+  this.refs.comment.value = '';
+  this.refs.price.value = '0.00';
+  return;
+ },
  render: function(){
   return(
-   {}
+   <form className="groceryForm" onSubmit={this.handleSubmit}>
+    <input type="text" placeholder="Grocery item" ref="name"/>
+    <input type="number" placeholder="0.00" min="0.01" step="0.01" ref="price"/>
+    <input type="text" placeholder="additional comments about this item" ref="comment"/>
+    <input type="submit" value="Add item" />
+   </form>
   );
  }
 });
 
 var GroceryBox = React.createClass({
+ getInitialState: function(){
+  return {data: []};
+ },
+ handleGrocerySubmit: function(grocery) {
+   var groceries = this.state.data;
+   var newGroceries = groceries.concat([grocery]);
+   this.setState({data: newGroceries});
+    $.ajax({
+     url: this.props.url,
+     dataType: 'json',
+     type: 'POST',
+     data: grocery,
+     success: function(data){
+      this.setState({data: data});
+     }.bind(this),
+     error: function(xhr, status, err){
+      console.error(this.props.url, status, err.toString());
+     }.bind(this)
+    });
+  },
+ loadGroceriesFromServer: function(){
+  $.ajax({
+   url: this.props.url,
+   dataType: 'json',
+   cache: false,
+   success: function(data){
+    this.setState({data: data});
+   }.bind(this),
+   error: function(xhr, status, err){
+    console.error(this.props.url, stats, err.toString());
+   }.bind(this)
+  });
+ },
+ componentDidMount: function(){
+  this.loadGroceriesFromServer();
+  setInterval(this.loadGroceriesFromServer, this.props.pollInterval);
+ },
  render: function(){
   return(
-   <div className="commentBox">
+   <div className="groceryBox">
     <h1>Groceries</h1>
-    <GroceryList data={this.props.data}/>
-    <GroceryForm />
+    <GroceryList data={this.state.data} />
+    <GroceryBox onGrocerySubmit={this.handleGrocerySubmit} />
    </div>
   );
  }
 });
 
-var data = [
- {name: "soup", price: "0.99", comment: "I like soup"},
- {name: "nutella", price: "9.99", comment: "I like nutella!!!"},
- {name: "carrots", price: "3", comment: "carrots tho"},
-];
-
 ReactDOM.render(
- <GroceryList data={data}/>,
+ <GroceryBox url="/api/grocerylist" pollInterval={2000} />,
  document.getElementById('content')
 );
